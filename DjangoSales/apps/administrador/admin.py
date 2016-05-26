e28fc37b95f6277e028574b67aeb6d32ba5a8dad
ifonseca <lamgers@gmail.com>
2016-05-26 16:00:57 -0500

    agregando modelos

--- DjangoSales/apps/administrador/admin.py
+++ DjangoSales/apps/administrador/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import(
 	Proveedor,
 	Entradas,
-	Inventario
+	Inventario,
+	CatalogoCategoria
 	)
 
 admin.site.register(Proveedor)
 admin.site.register(Entradas)
 admin.site.register(Inventario)
+admin.site.register(CatalogoCategoria)
