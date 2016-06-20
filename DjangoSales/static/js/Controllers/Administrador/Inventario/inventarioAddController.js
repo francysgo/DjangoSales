9b8e45322dd64f56c1d9b80912974b0769951197
ifonseca <lamgers@gmail.com>
2016-06-19 22:52:06 -0500

    Terminando inventario

--- DjangoSales/static/js/Controllers/Administrador/Inventario/inventarioAddController.js
+++ DjangoSales/static/js/Controllers/Administrador/Inventario/inventarioAddController.js
@@ -1,12 +1,17 @@
 SalesApp.controller('inventarioAddController', function($scope, $http) {
-
+	$scope.productos_actualizar = [];
+	
 	$scope.addProducto = function (){
 		console.log($scope.upc_actualizar);
 		var url = '/api/inventario.json?producto__upc=' + $scope.upc_actualizar;
-		$scope.productos_actualizar = {};
+		$('#actual_upc').val('');
 		$http.get(url)
 	    .success(function(data) {
-	            $scope.productos_actualizar.push(data);
+	    	console.log(data);
+	            $scope.productos_actualizar.push({
+	            	'upc': data[0].producto.upc,
+	            	'nombre':data[0].producto.nombre,
+	            });
 	        })
 	    .error(function(data) {
 	            console.log('Error: ' + data);
