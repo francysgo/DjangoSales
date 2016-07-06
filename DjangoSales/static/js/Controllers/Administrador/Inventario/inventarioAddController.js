f3dc7330e9c997d82b407f1db43ad0c8c1f4815d
isfon <lamgers@gmail.com>
2016-07-06 16:12:01 -0500

    Terminando inventario

--- DjangoSales/static/js/Controllers/Administrador/Inventario/inventarioAddController.js
+++ DjangoSales/static/js/Controllers/Administrador/Inventario/inventarioAddController.js
@@ -1,20 +1,45 @@
 SalesApp.controller('inventarioAddController', function($scope, $http) {
 	$scope.productos_actualizar = [];
-	
+	$scope.mostrarBoton = {}
+	$scope.mostrarBoton.show = false;
 	$scope.addProducto = function (){
-		console.log($scope.upc_actualizar);
 		var url = '/api/inventario.json?producto__upc=' + $scope.upc_actualizar;
 		$('#actual_upc').val('');
 		$http.get(url)
 	    .success(function(data) {
-	    	console.log(data);
-	            $scope.productos_actualizar.push({
+	    	var productos = $scope.productos_actualizar;
+	    	var iguales = 0;
+	    	if(productos.length > 0){
+	    		$scope.mostrarBoton.show = true;
+	    		for (var i=0; i < productos.length; i++) {
+	    			if(data[0].producto.upc == productos[i].upc){
+	    				iguales += 1;
+	    			}
+	    		}
+	    		if(iguales == 0){
+					$scope.productos_actualizar.push({
+		            	'upc': data[0].producto.upc,
+		            	'nombre':data[0].producto.nombre,
+		            	'cantidad':data[0].cantidad,
+		            });
+	    		}
+	    	}else{
+	    		$scope.mostrarBoton.show = true;
+	    		$scope.productos_actualizar.push({
 	            	'upc': data[0].producto.upc,
 	            	'nombre':data[0].producto.nombre,
+	            	'cantidad':data[0].cantidad,
 	            });
-	        })
+	    	}
+	     })
 	    .error(function(data) {
 	            console.log('Error: ' + data);
-	    });
+	    });  
+	}
+	$scope.deleteProducto = function (idx){
+	    $scope.productos_actualizar.splice($scope.productos_actualizar.indexOf(idx), 1);
+	    if($scope.productos_actualizar.length == 0){
+			$scope.mostrarBoton.show = false;
+	    }
 	}
 });
