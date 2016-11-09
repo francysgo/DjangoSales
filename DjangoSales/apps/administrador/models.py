a4525b6180b5a6d9a4ecd4dde7493d53e275ca4e
isfon <lamgers@gmail.com>
2016-11-08 23:17:01 -0600

    working with new interface and angular components

--- DjangoSales/apps/administrador/models.py
+++ DjangoSales/apps/administrador/models.py
@@ -124,8 +124,3 @@ class DescripcionVenta(models.Model):
 
 	def __str__(self):
 		pass
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
--- DjangoSales/static/js/app.js
+++
@@ -1,8 +0,0 @@
-var SalesApp = angular.module('SalesApp', ['angularUtils.directives.dirPagination','angular-loading-bar','ngRoute']);
-
-	SalesApp.config(function($interpolateProvider,$httpProvider) {
-	    $interpolateProvider.startSymbol('[[');
-	    $interpolateProvider.endSymbol(']]');
-	  	$httpProvider.defaults.xsrfCookieName = 'csrftoken';
-     	$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-	 });
